d82a61d6a671bbde4c637952dd3cf69f95dcbd49
guanwei <guanwei@>
2026-05-11 17:43:32 +0800

    ADD 子模块引用

--- res/vcpkg/ffmpeg/portfile.cmake
+++ res/vcpkg/ffmpeg/portfile.cmake
@@ -27,7 +27,7 @@ vcpkg_from_github(
     patch/0009-fix-nvenc-reconfigure-blur.patch
     patch/0010.disable-loading-DLLs-from-app-dir.patch
     patch/0011-android-mediacodec-encode-align-64.patch
-    patch/0012-fix-macos-big-sur-CVBufferCopyAttachments.patch
+    # patch/0012-fix-macos-big-sur-CVBufferCopyAttachments.patch  # macOS only, not needed for Windows/Android
 )
 
 if(SOURCE_PATH MATCHES " ")
